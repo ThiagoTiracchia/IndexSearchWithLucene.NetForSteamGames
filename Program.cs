18003f4e06be0da1f3b2d4c7e5d5b09484010eba
thiago.tiracchia <thiago.tiracchia@gmail.com>
2024-06-06 11:25:47 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using IndexSearch;
+using Newtonsoft.Json.Linq;
+using SteamSearch;
+using System.Text.Json;
+
+
+namespace SteamSearch
+{
+    public class SteamGame
+    {
+        public string name { get; set; }
+
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var SteamFileText = File.ReadAllText("C:\\Users\\thiag\\source\\repos\\IndexSearch\\steamGames.json");
+        var values = JObject.Parse(SteamFileText).SelectToken("applist").SelectToken("apps");
+        var averdevuelta = values.ToString();
+        var steamgames = JsonSerializer.Deserialize<List<SteamGame>>(averdevuelta);
+
+
+        var engine = new SteamSearchEngine();
+        engine.AddSteamGamesToIndex(steamgames);
+        RunConsole(engine);
+
+    }
+
+    private static void RunConsole(SteamSearchEngine engine)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a search query: ");
+            var query = Console.ReadLine();
+            if (string.IsNullOrEmpty(query))
+                continue;
+
+            var results = engine.SearchSteamGames(query);
+            if (!results.Any())
+            {
+                Console.WriteLine("No results Found");
+                continue;
+
+            }
+
+            foreach (var result in results)
+            {
+                Console.WriteLine($"({result.name})");
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
+/* var values = JObject.Parse(SteamFileText).SelectToken("applist").SelectToken("apps").ToArray();
+       List<string> listOfNames = new List<string>();
+       foreach (var item in values)
+       {
+
+           var nameOfGame =  item.Value<string>("name");
+           if (nameOfGame == "" || nameOfGame.Contains("test")  ) {
+
+
+           } else
+           {
+               listOfNames.Add(nameOfGame);
+
+           }
+
+
+       }
+       */
---
+++ SteamSearchEngine.cs
@@ -0,0 +1,79 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using LuceneDirectory = Lucene.Net.Store.Directory;
+using Document = Lucene.Net.Documents.Document;
+using Lucene.Net.QueryParsers.Classic;
+using SteamSearch;
+using Lucene.Net.Search.Suggest.Analyzing;
+
+
+
+
+namespace IndexSearch
+{
+
+    public class SteamSearchEngine
+    {
+        private readonly AnalyzingInfixSuggester suggester;
+        private readonly Analyzer _analyzer;
+        private readonly LuceneDirectory _directory;
+        private readonly IndexWriter _writer;
+
+        private const LuceneVersion version = LuceneVersion.LUCENE_48;
+
+        public SteamSearchEngine()
+        {
+           
+            _analyzer = new StandardAnalyzer(version);
+            _directory = new RAMDirectory();
+            var config = new IndexWriterConfig(version, _analyzer);
+            _writer = new IndexWriter(_directory, config);
+            AnalyzingInfixSuggester suggester = new AnalyzingInfixSuggester(
+               version, _directory, _analyzer);
+
+
+        }
+
+        public void AddSteamGamesToIndex(IEnumerable<SteamGame> steamGames)
+        {
+            foreach (SteamGame steam in steamGames) { 
+                var document = new Document();
+                document.Add(new TextField("name", steam.name, Field.Store.YES));
+            
+                _writer.AddDocument(document);
+            }
+            _writer.Commit();
+
+
+        }
+
+        public IEnumerable<SteamGame> SearchSteamGames(string searchterm) { 
+        var directoryReader =  DirectoryReader.Open(_directory);
+        var indexSearcher = new IndexSearcher(directoryReader);
+
+            string  field = "name";
+            QueryParser parser = new QueryParser(version, field, _analyzer);
+            Query query = parser.Parse(searchterm);
+            var hits = indexSearcher.Search(query, n: 10).ScoreDocs;
+            var games = new List<SteamGame>();
+            foreach(var hit in hits)
+            {
+                var document = indexSearcher.Doc(hit.Doc);
+                games.Add(new SteamGame()
+                {
+                    name = document.Get("name")
+                }); ;
+
+            }
+            return games;
+
+
+        }
+
+    }
+}
